f188d932eabd1f3a6bfbda5330ff1da7dc8fc984
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2018-03-04 15:10:28 -0800

    create visuals. render player and render ball methods. render objects onload

---
+++ scripts/app.js
@@ -0,0 +1,49 @@
+var canvas = document.getElementById('game-canvas');
+var context = canvas.getContext('2d');
+
+
+// Board
+context.fillStyle = 'black';
+context.fillRect(25, 25, 600, 450);
+
+// Player Object constructor
+function Player(x, y) {
+  this.x = x;
+  this.y = y;
+  this.width = 25;
+  this.height = 100;
+  this.color = 'blue';
+  this.render = function () {
+    context.fillStyle = this.color;
+    context.fillRect(this.x, this.y, this.width, this.height);
+  };
+}
+
+// Declare new players and ball
+var human = new Player(30,200);
+var computer = new Player(595, 200);
+var ball = new Ball(325, 250);
+
+function Ball(x, y) {
+  this.x = x;
+  this.y = y;
+  this.color = 'white';
+  this.render = function () {
+    context.beginPath();
+    context.fillStyle = this.color;
+    context.arc(this.x, this.y, 10, 0, Math.PI * 2, false);
+    context.fill();
+  };
+}
+
+// Render method to load paddles and ball
+var render = function() {
+  ball.render();
+  human.render();
+  computer.render();
+};
+
+// Render objects on window load
+window.onload = function() {
+  render();
+};
