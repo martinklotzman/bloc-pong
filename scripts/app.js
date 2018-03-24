aa0876510350dccac2bccd582956538523c55dde
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2018-03-24 10:40:52 -0700

    checkCollision method now lets ball collide with paddles

--- scripts/app.js
+++ scripts/app.js
@@ -7,11 +7,11 @@ context.fillStyle = 'black';
 context.fillRect(0, 0, canvas.width, canvas.height);
 
 // Player Object constructor
-function Player(x, y) {
+function Player(x, y, width, height) {
   this.x = x;
   this.y = y;
-  this.width = 25;
-  this.height = 110;
+  this.width = width;
+  this.height = height;
   this.color = 'blue';
   this.speed = 10;
 
@@ -24,7 +24,7 @@ function Player(x, y) {
 
   this.render = function () {
     context.fillStyle = this.color;
-    context.fillRect(this.x, this.y, this.width, this.height);
+    context.fillRect(this.x, this.y, width, height);
   };
 }
 
@@ -44,56 +44,53 @@ Player.prototype.move = function(event) {
 }
 
 // Declare new players and ball
-var human = new Player(0, 200);
-var computer = new Player(625, 190);
-var ball = new Ball(325, 250);
+var human = new Player(0, 200, 25, 110);
+var computer = new Player(625, 190, 25, 110);
+var ball = new Ball(canvas.width / 2, canvas.height / 2);
 
 // Ball Object constructor
 function Ball(x, y) {
   this.x = x;
   this.y = y;
   this.color = 'white';
-  this.x_speed = -3;
-  this.y_speed = 0;
+  this.radius = 10;
+  this.x_speed = -2;
+  this.y_speed = -1;
 }
 
 Ball.prototype.render = function() {
   context.beginPath();
   context.fillStyle = this.color;
-  context.arc(this.x, this.y, 10, 0, Math.PI * 2, false);
+  context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
   context.fill();
 }
 
 Ball.prototype.move = function(paddle1, paddle2) {
   this.x += this.x_speed;
-  this.y -= this.y_speed;
-  var top_x = this.x - 5;
-  var top_y = this.y - 5;
-  var bottom_x = this.x + 5;
-  var bottom_y = this.y + 5;
+  this.y += this.y_speed;
 
-  if(this.y - 5 < 0) { //hitting the top wall
-    this.y = 5;
+  if (this.y - 5 < 0) {
     this.y_speed = -this.y_speed;
-  } else if (this.y + 5 > 500) { //hitting the bottom wall
-    this.y = 495;
+  } else if (this.y + 5 > canvas.height) {
     this.y_speed = -this.y_speed;
   }
-// move collusion into its own protoype function?
-  if((paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
-    // hit the player's paddle
-    this.y_speed = 0;
-    this.x_speed += (paddle1.speed / 2);
-    // this.y += this.y_speed;
-    console.log("human");
-  }
 
-  if((paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
-    // hit the computer's paddle
-    this.y_speed = 5;
-    this.x_speed -= (paddle2.speed / 2);
-    // this.y -= this.y_speed;
-    console.log("computer");
+  function checkCollision(paddle, axis) {
+    var widthheight = axis ==  "x" ? "width" : "height";
+    var rectPos = paddle[axis] + paddle[widthheight] / 2;
+    return Math.abs(ball[axis] - rectPos);
+  };
+
+  var playerX = checkCollision(human, "x");
+  var playerY = checkCollision(human, "y");
+
+  var computerX = checkCollision(computer, "x");
+  var computerY = checkCollision(computer, "y");
+
+  if(playerX < (human.width/2 + ball.radius) && playerY < (human.height/2 + ball.radius)) {
+    this.x_speed = -this.x_speed;
+  } else if (computerX < (computer.width / 2 + ball.radius) && computerY < (computer.height / 2 + ball.radius)) {
+    this.x_speed = -this.x_speed;
   }
 }
 
@@ -119,7 +116,7 @@ var render = function() {
   human.render();
   computer.render();
   ball.render();
-  ball.move(human, computer);
+  ball.move();
 };
 
 // Render objects on window load
