e42df2a413e44de3ea1fc372adf2ebe27fe5f03d
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2018-03-30 14:24:16 -0700

    added computer ai to paddle and ball resets after moving past paddle

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,12 @@
 var canvas = document.getElementById('game-canvas');
 var context = canvas.getContext('2d');
 
-
 // Board
 context.fillStyle = 'black';
 context.fillRect(0, 0, canvas.width, canvas.height);
 
 // Player Object constructor
-function Player(x, y, width, height) {
+function Paddle(x, y, width, height) {
   this.x = x;
   this.y = y;
   this.width = width;
@@ -15,38 +14,55 @@ function Player(x, y, width, height) {
   this.color = 'blue';
   this.speed = 10;
 
-  // Prevents Player Object from leaving game board
-  this.boardEdge  = function(direction) {
-    if(this.y + direction >= 0 && this.y + this.height + direction <= canvas.height) {
-      this.y += direction;
-    }
-  };
+};
 
-  this.render = function () {
-    context.fillStyle = this.color;
-    context.fillRect(this.x, this.y, width, height);
-  };
+Paddle.prototype.move = function(direction) {
+  if(this.y + direction >= 0 && this.y + this.height + direction <= canvas.height) {
+    this.y += direction;
+  }
+};
+
+Paddle.prototype.render = function() {
+  context.fillStyle = this.color;
+  context.fillRect(this.x, this.y, this.width, this.height);
+};
+
+// press key event listener
+function addKeyEvent() {
+  window.addEventListener('keydown', keyPress, true);
 }
 
-// Player paddle controls
-Player.prototype.move = function(event) {
-  let keyCode = event.keyCode;
-  switch(keyCode) {
+// players key press movement
+function keyPress(direction) {
+  switch (direction.keyCode) {
     case 38:
-      this.human.boardEdge(-human.speed);
+      human.move(-human.speed); // up
       break;
     case 40:
-      this.human.boardEdge(human.speed);
+      human.move(human.speed); // down
       break;
-    default:
-      break;
-    }
+  }
 }
 
 // Declare new players and ball
-var human = new Player(0, 200, 25, 110);
-var computer = new Player(625, 190, 25, 110);
+var human = new Paddle(0, 200, 25, 110);
+var computer = new Paddle(625, 190, 25, 110);
 var ball = new Ball(canvas.width / 2, canvas.height / 2);
+
+// Computer AI
+computer.update = function(ball) {
+  var ball_y_position = ball.y;
+  var diff = -((computer.y + (computer.width / 2)) - ball_y_position)
+
+  if(diff < 0) {
+    diff = -2;
+  } else if(diff > 0) {
+    diff = 2;
+  }
+  // Sets the diffictulty
+  computer.move(diff * .45);
+};
+
 
 // Ball Object constructor
 function Ball(x, y) {
@@ -54,16 +70,31 @@ function Ball(x, y) {
   this.y = y;
   this.color = 'white';
   this.radius = 10;
-  this.x_speed = -2;
+  this.x_speed = -3;
   this.y_speed = -1;
-}
+
+  this.resetPosition = function() {
+    this.x = canvas.width / 2;
+    this.y = canvas.height / 2;
+  };
+
+  this.resetSpeed = function() {
+    this.x_speed = -3;
+    this.y_speed = -1;
+  };
+
+  this.reset = function() {
+    this.resetSpeed();
+    this.resetPosition();
+  };
+};
 
 Ball.prototype.render = function() {
   context.beginPath();
   context.fillStyle = this.color;
   context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
   context.fill();
-}
+};
 
 Ball.prototype.move = function(paddle1, paddle2) {
   this.x += this.x_speed;
@@ -92,7 +123,26 @@ Ball.prototype.move = function(paddle1, paddle2) {
   } else if (computerX < (computer.width / 2 + ball.radius) && computerY < (computer.height / 2 + ball.radius)) {
     this.x_speed = -this.x_speed;
   }
-}
+};
+
+Ball.prototype.update = function() {
+  if(this.x < 0) {
+    this.reset();
+  } else if(this.x > 650) {
+    this.reset();
+  }
+
+  // if(rightScored || leftScored) {
+  //   if(rightScored) {
+  //     rightScored++;
+  //   }
+  //   if(leftScored) {
+  //     leftScored++;
+  //   }
+  //   this.reset();
+  // }
+
+};
 
 // Continuously renders the view during gameplay
 var animate = window.requestAnimationFrame || function(callback) { window.setTimeout(callback, 1000/60) };
@@ -102,14 +152,10 @@ var step = function() {
   context.fillStyle = 'black';
   context.fillRect(0, 0, canvas.width, canvas.height);
 
+  update();
   render();
   animate(step);
 };
-
-// Event listener for paddle movement
-function addEventKey() {
-  window.addEventListener("keydown", human.move);
-}
 
 // Render method to load players and ball
 var render = function() {
@@ -117,10 +163,16 @@ var render = function() {
   computer.render();
   ball.render();
   ball.move();
+  // drawScore();
+};
+
+var update = function() {
+  computer.update(ball);
+  ball.update();
 };
 
 // Render objects on window load
 window.onload = function() {
-  addEventKey();
+  addKeyEvent();
   step();
 };
